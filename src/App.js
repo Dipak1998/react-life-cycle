f3be9869745ffb3c0d7cff60af55b389447cc8f7
Dipak Garai <garaidipak1998@gmail.com>
2021-10-02 19:31:20 +0530

    Life-cycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    // intilialization  ---- we initialize state and props
+    console.log('Constructor call')
+    super()
+    this.state = {
+      isOn: false,
+    }
+  }
+  componentWillMount() {
+    console.log('Component Will Mount Call')
+  }
+  componentDidMount() {
+    // we are calling the apis means fethcing data from server
+    console.log('Component Did Mount')
+  }
+  render() {
+    console.log('Render Call')
+    return (
+      <div>
+        <h1>We are learning Life Cycle Methods in Class Component</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
